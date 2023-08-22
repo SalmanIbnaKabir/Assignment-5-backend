03d60d3bd82dbaeb9913a61c56d9a5f27bd73864
SalmanIbnaKabir <salmanibnakabir@gmail.com>
2023-08-22 23:33:17 +0600

    login add success

--- src/module/user/user.controller.js
+++ src/module/user/user.controller.js
@@ -26,6 +26,33 @@ const signup = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  try {
+    const { ...loginData } = req.body;
+
+    const result = await UserService.login(loginData);
+    if (!result) {
+      return res.status(404).json({
+        success: false,
+        message: "login not successful",
+      });
+    }
+
+    res.status(201).json({
+      success: true,
+      message: "signup successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "login not successful server error",
+      errorMessage: error.message,
+    });
+  }
+};
+
 export const UserController = {
   signup,
+  login,
 };
--- src/module/user/user.service.js
+++ src/module/user/user.service.js
@@ -1,11 +1,32 @@
 import { User } from "./user.model.js";
+import bcrypt from "bcrypt";
 
 const signup = async (payload) => {
-  const result = await User.create(payload);
+  const result = await User.create(payload, { password: 0 });
 
   return result;
 };
 
+const login = async (payload) => {
+  const { email, password } = payload;
+  console.log(email, password);
+
+  const findUser = await User.findOne({ email: email });
+  console.log(findUser);
+  if (!findUser) {
+    return false;
+  }
+
+  const passwordMatch = await bcrypt.compare(password, findUser.password);
+
+  if (!passwordMatch) {
+    return false;
+  }
+
+  return findUser;
+};
+
 export const UserService = {
   signup,
+  login,
 };
