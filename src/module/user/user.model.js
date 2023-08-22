0bf15480729d8706effe6a8262f54a13d2e73a30
SalmanIbnaKabir <salmanibnakabir@gmail.com>
2023-08-22 22:40:28 +0600

    user model and other modlue add

---
+++ src/module/user/user.model.js
@@ -0,0 +1,24 @@
+import mongoose, { model } from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = model("User", userSchema);
